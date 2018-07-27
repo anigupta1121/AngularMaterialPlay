848743e482ff44cba14b7c4c80f15cb9c861fcf2
Anirudh Gupta <anirudh.gupta@newgen.co.in>
2018-07-27 14:52:36 +0530

    SignUp form complete

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl ,FormGroup} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
-import {User} from './../User'
+import { User } from './../User'
 
 @Component({
   selector: 'app-sign-up',
@@ -10,32 +10,53 @@ import {User} from './../User'
 })
 export class SignUpComponent implements OnInit {
 
-  email=  new FormControl('bob@example.com', Validators.required);
-  constructor() { }
 
-   //Gender list for the select control element
-   private genderList: string[];
-   //Property for the user
-   private user:User;
+  constructor(private fb: FormBuilder) { }
 
-   signupForm: FormGroup;
-  
-   ngOnInit() {
-  
-     this.genderList =  ['Male', 'Female', 'Others'];
+  //Gender list for the select control element
+  private genderList: string[];
+  //Property for the user
+  private user: User;
 
-     this.signupForm = new FormGroup ({
+  signupForm: FormGroup;
+
+  ngOnInit() {
+
+    this.genderList = ['Male', 'Female', 'Others'];
+
+    this.signupForm = this.fb.group({
       email: new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]),
       password: new FormGroup({
-        pwd: new FormControl('',Validators.required),
-        confirmPwd: new FormControl('',Validators.required)
+        pwd: new FormControl('', [Validators.required, Validators.minLength(8)]),
+        confirmPwd: new FormControl('', [Validators.required, Validators.minLength(8)])
       }),
-      
-      gender: new FormControl('',Validators.required),
-      terms: new FormControl('',Validators.required)
-  })
-    
-     
- }
+
+      gender: new FormControl('', Validators.required),
+      terms: new FormControl('', Validators.required)
+    })
+
+
+
+  }
+
+
+  public onFormSubmit() {
+    if (this.signupForm.valid) {
+      this.user = this.signupForm.value;
+      console.log(this.user);
+
+      if(this.user.password.pwd!=this.user.password.confirmPwd)
+       alert('Passwords do not match');
+      /* Any API call logic via services goes here */
+    }
+  }
+
+  get email() { return this.signupForm.get('email'); }
+
+  get password() { return this.signupForm.get('password'); }
+
+  get gender() { return this.signupForm.get('gender'); }
+
+  get terms() { return this.signupForm.get('terms'); }
 
 }
